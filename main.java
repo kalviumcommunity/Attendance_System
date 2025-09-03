f8233609f8cd316571e5346e561d74ec58904bb9
nidhi namdev <nidhi.namdev@kalvium.community>
2025-09-03 11:13:35 +0530

    setup

--- main.java
+++ main.java
@@ -3,6 +3,8 @@ package com.school;
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello!, Attendance Management System");
+        System.out.println("Welcome to the Attendance Management System");
+        
     }
     
 }
